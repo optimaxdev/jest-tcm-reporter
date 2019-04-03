29c216121f2df0c7ff68d92144117b1ae2690e7e
Vladislav Korolev <woked@live.ru>
2019-04-03 17:34:29 +0400

    ANT-345 move #!/usr/bin/env node to bin/index

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const { createTestCycles, createTestCyclesByDescribe } = require('../scripts/');
 
 (function run() {
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 require('@babel/register');
 const fs = require('fs-extra');
 const { config } = require('../config/config');
